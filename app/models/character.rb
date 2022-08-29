b97a130e5e813926818c8e791a467b5c475f5e66
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-29 23:32:23 +0300

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    say = self.name + " always says:"
+    that_thing = say + " "+ self.catchphrase
+    that_thing
+  end  
 end
